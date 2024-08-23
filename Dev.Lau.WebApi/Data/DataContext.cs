52c4eef6fd89cfad9d0fc1a6be0eb951dc692251
Laurens van Dijk <laurensrobertvandijk@gmail.com>
2024-08-23 15:15:12 +0200

    Remove tryals on disk

--- Dev.Lau.WebApi/Data/DataContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Dev.Lau.WebApi.Data;
-
-public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext(options)
-{
-}
